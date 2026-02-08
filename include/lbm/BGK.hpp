1886f8c9d4c5c5a423fe7befcf0b04af1a9709dd
ToruNiina <niina.toru.68u@gmail.com>
2026-02-09 01:18:08 +0900

    feat: add bgk

---
+++ include/lbm/BGK.hpp
@@ -0,0 +1,60 @@
+#ifndef LATTICE_BOLTZMANN_BGK_HPP
+#define LATTICE_BOLTZMANN_BGK_HPP
+
+namespace lbm
+{
+
+struct BGK
+{
+    explicit BGK(const double nu)
+        : viscosity_(nu), omega_(1.0 / (3.0 * nu + 0.5))
+    {}
+
+    Grid collide(const Grid& g) const
+    {
+        Grid next;
+
+        const auto rho = g.density();
+        const auto u   = g.velocity(rho);
+
+        for(const auto& dir : all_dirs)
+        {
+            next.distribution(dir) =
+                (1 - omega_) * g.distribution(dir) +
+                this->equilibrium(dir, rho, u);
+        }
+        return ;
+    }
+
+    double equilibrium(const Direction dir, const double rho, const Vector u) const
+    {
+        const auto u2 = length_sq(u);
+
+        using enum Direction;
+        switch(dir)
+        {
+            case Self     : { return (4/ 9.0) * rho * (1 - 1.5 * u2); }
+
+            case Right    : { return (1/ 9.0) * rho * (1 + 3*u.x + 2/9.0*u.x*u.x - 1.5*u2); }
+            case Up       : { return (1/ 9.0) * rho * (1 + 3*u.y + 2/9.0*u.y*u.y - 1.5*u2); }
+            case Left     : { return (1/ 9.0) * rho * (1 - 3*u.x + 2/9.0*u.x*u.x - 1.5*u2); }
+            case Down     : { return (1/ 9.0) * rho * (1 - 3*u.y + 2/9.0*u.y*u.y - 1.5*u2); }
+
+            case RightUp  : { return (1/36.0) * rho * (1 + 3*( u.x+u.y) + 2/9.0*( u.x+u.y)*( u.x+u.y) - 1.5 * u2); }
+            case LeftUp   : { return (1/36.0) * rho * (1 + 3*(-u.x+u.y) + 2/9.0*(-u.x+u.y)*(-u.x+u.y) - 1.5 * u2); }
+            case LeftDown : { return (1/36.0) * rho * (1 + 3*(-u.x-u.y) + 2/9.0*(-u.x-u.y)*(-u.x-u.y) - 1.5 * u2); }
+            case RightDown: { return (1/36.0) * rho * (1 + 3*( u.x-u.y) + 2/9.0*( u.x-u.y)*( u.x-u.y) - 1.5 * u2); }
+            default: break;
+        }
+        assert(false);
+        return 0;
+    }
+
+  private:
+
+    double viscosity_;
+    double omega_; // 1/tau
+};
+
+} // lbm
+#endif // LATTICE_BOLTZMANN_BGK_HPP
